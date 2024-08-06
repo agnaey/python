ce030b3185ea9666c1b5b663fa6d09c8d1e77080
agnaey <agnaeyviswanath1234@gmail.com>
2024-08-06 17:24:28 +0530

    variables

---
+++ demo2.py
@@ -0,0 +1,35 @@
+# name=input("ener your name : ")
+# age=int(input("enter your age :"))
+# weight=float(input("enter your weight : "))
+
+# print("name",name)
+# print("age",age)
+# print("weight",weight)
+
+
+# a="123"
+# print("a",type(a),a)
+# b=int(a)
+# print("b",type(b),b)
+# c=float(a)
+# print("c",type(c))
+
+
+# print("hello \nevery \none")
+# print("""how
+# do 
+# you 
+# do""")
+
+# print('''\ti
+#       am
+#       fine
+#       and you''')
+# print("hi\tguys\thow")
+
+item=input("Enter your item : ")
+price=int(input("enter tour price : "))
+
+print(f"i have a {item},it cost {price}")
+
+

1d84b19127d138c758afb0d1afb318b384f59bd7
agnaey <agnaeyviswanath1234@gmail.com>
2024-08-07 16:07:46 +0530

    opeators added

---
+++ operators.py
@@ -0,0 +1,119 @@
+# ----------Arithmetic opreators-----------
+
+# print("agnaey "+"viswanath")
+
+# print(10-5)
+
+# print(10/5)
+
+# print(10*5)
+
+# print(10%5)
+
+# print(10**5)
+
+# print(10//5)
+
+# -------------assigment operator------------------
+
+# a=5
+# print(a)
+
+# a=10
+# a+=5
+# print(a)
+
+# a=15
+# a-=5
+# print(a)
+
+# a=10
+# a*=5
+# print(a)
+
+# a=10
+# a/=5
+# print(a)
+
+# a=10
+# a%=5
+# print(a)
+
+# a=10
+# a//=5
+# print(a)
+
+# a=10
+# a**=5
+# print(a)
+
+# ---------comparison operators-------------------
+
+# print(10==10)
+# print(10==9)
+
+# print(10!=9)
+# print(10!=10)
+# print(10!=11)
+
+# print(10>11)
+# print(10>9)
+
+# print(10<9)
+# print(10<11)
+
+# print(10<=10)
+# print(10<=11)
+# print(10<=9)
+
+# print(10>=10)
+# print(10>=11)
+# print(10>=9)
+
+
+# --------logical operator----------------------
+
+# and
+# print(10==10 and 10>=9)
+# print(10==10 and 10>=11)
+
+# or
+# print(10==10 or 10>=9)
+# print(10==5 or 10>=11)
+# print(10==5 or 10>=9)
+
+# not
+# print(not(10==10))
+# print(not(10==9))
+
+# -------membership---------------
+
+# in
+# print('e'in 'welcome')
+# print('a'in 'welcome')
+
+# not in
+# print('e' not in "welcome")
+# print('a' not in "welcome")
+
+# -----------identity operator----------------
+
+#is
+# a=10
+# b=10
+# print(a==b)
+# print(a is b)
+# print(id(a))
+# print(id(b))
+
+# a=7
+# b=11
+# print(a is b)
+# print(id(a))
+# print(id(b))
+
+# list=[10,20]
+# list1=[10,20]
+# print(list is list1)
+# print(id(list))
+# print(id(list1))

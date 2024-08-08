5e6346d5b43eb428e22b1fb4b2cc1ed1e6cc9586
agnaey <agnaeyviswanath1234@gmail.com>
2024-08-08 16:09:58 +0530

    string and task1

---
+++ conditional/task1.py
@@ -0,0 +1,13 @@
+# a=int(input("enter your number"))
+# b=int(input("enter your number"))
+# if a==b:
+#     print("true")
+# else:
+#     print("false")
+
+a=int(input("enter your no"))
+b=int(input("enter your no"))
+if a>b:
+    print("a is greater")
+else:
+    print("b is greater")
---
+++ string.py
@@ -0,0 +1,39 @@
+# a="welcome"
+# print(a[0])
+# print(a[4])
+# print(a[6])
+
+#slicing
+# a="welcome"
+# print(a[:4])
+# print(a[2:])
+
+# increment
+# a="welcome"
+# print(a[::2])
+# print(a[::-1])
+# print(a[2:6:2])
+
+
+# a="welcome"
+# print(a.capitalize())
+
+# print(a.center(20))
+
+# print(a.count('o'))
+
+# print(a.endswith('e'))
+# print(a.endswith('w'))
+
+# print(a.find("e"))
+# print(a.find('m'))
+
+# print(a.index('c'))
+
+# b='1234'
+# print(a.isdigit())
+# print(b.isdigit())
+
+print("{:<10}{:<8} {:<8}".format("name","age","mark"))
+print("{:<10}{:<8}{:<8}".format("agnaey","21","50"))
+print("{:<10}{:<8}{:<8}".format("akshay",'20','49'))

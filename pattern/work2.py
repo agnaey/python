93bd576fd627549f795510a01a65c7f8ee19f1a8
agnaey <agnaeyviswanath1234@gmai.com>
2024-08-17 15:58:40 +0530

    pattern chr

---
+++ pattern/work2.py
@@ -0,0 +1,76 @@
+# for i in range(1,4):
+#     for j in range(i):
+#         print(j,end=" ")
+#     print()
+
+'''
+0 
+0 1 
+0 1 2 
+'''
+
+
+
+# a=0
+# for i in range(1,4):
+#     for j in range(i):
+#         print(a,end=" ")
+#         a+=1
+#     print()
+
+
+
+'''
+0 
+1 2 
+3 4 5 
+'''
+
+
+
+# a=0
+# for i in range(1,4):
+#     for j in range(i):
+#         print(a,end=" ")
+#     print()
+#     a+=1
+
+
+'''
+0 
+1 1 
+2 2 2 
+'''
+
+# a=0
+# for i in range (1,4):
+#     for j in range(i):
+#         print(a-j,end=" ")
+#     print()
+#     a+=1
+
+    
+
+
+'''
+0 
+1 0 
+2 1 0 
+'''
+
+
+for i in range (1,5):
+    if i%2==0:
+        for j in range (i):
+            print(j,end="  ")
+        print()
+    else:
+        for j in range (i):
+            print(2-j,end="  ")
+        print()
+
+'''
+2  
+0  1  
+2  1  0  
+'''

7e38ac38e7c4d1e2469587e0ff4420e9fd6fa58f
agnaey <agnaeyviswanath1234@gmail.com>
2024-08-20 16:07:14 +0530

    works

---
+++ list/work2,1_homework.py
@@ -0,0 +1,49 @@
+l=[1,2,3,4,5,6]
+x=0
+y=0
+for i in l:
+    if i%2==0:
+        x=x+1
+    else:
+        y=y+i
+print(x)
+print(y)
+
+
+
+
+
+
+
+
+
+
+
+
+# l=[10,11,5.8,'abc']
+# x=0
+# for i in l:
+#     if type(i)==int or type(i)==float:
+#         x=x+i
+# print(x)
+
+'''26.8'''
+
+
+
+# l=[1,2,5,8,9,11,16,12]
+# l1=[]
+# l2=[]
+# for i in l:
+#     if i%2==0:
+#         l1.append(i)
+# print(l1)
+# for i in l1:
+#     i=i**2
+#     l2.append(i)
+# print(l2)
+
+'''
+[2, 8, 16, 12]
+[4, 64, 256, 144]
+'''
---
+++ list/work2.py
@@ -0,0 +1,91 @@
+# l=[1,2,5,7,8]
+# x=0
+# for i in l:
+#     x=x+i
+# print(x)
+
+"""23"""
+
+
+# Names=[]
+# limit=int(input("how many stds: "))
+# for i in range (limit):
+#     name=input("enter name : ")
+#     Names.append(name)
+# print(Names)
+
+'''
+how many stds: 2
+enter name : agaey
+enter name : tutu
+['agaey', 'tutu']
+'''
+
+
+
+'''           remove duplicate values'''
+
+
+# l=[1,2,1,5,8,"abc",8,"abc"]
+
+# new=[]
+# for i in l:
+#     if i not in new:
+#         new.append(i)
+# print(new)
+
+'''  
+[1, 2, 5, 8, 'abc']
+'''
+
+
+"""           print the largest"""
+
+# l=[1,2,10,8,9,5]
+# large=l[0]
+# for i in l:
+#     if i>large:
+#         large=i
+# print(large)
+
+'''
+10
+'''
+
+
+
+"""           print the smallest value"""
+# l=[1,2,10,8,9,5]
+# small=l[0]
+# for i in l:
+#     if i<small:
+#         small=i
+# print(small)
+
+'''
+1
+'''
+
+"""           reverse of content"""
+
+# l=['apple','orange','mango','kiwi']
+# for i in l:
+#     print(i[::-1])
+
+# --------------------------------
+
+# l=['apple','orange','mango','kiwi']
+# for i in l:
+#     rev=''
+#     for j in i :
+#         rev=j+rev
+#     print(rev)
+
+
+'''
+elppa
+egnaro
+ognam
+iwik
+'''
+

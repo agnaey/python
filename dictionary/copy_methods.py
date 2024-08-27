b1fdcedfdd095dbfcabcbcc0277fe7354797b633
agnaey <agnaeyviswanath1234@gmail.com>
2024-08-27 16:03:29 +0530

    new work

--- dictionary/copy_methods.py
+++ dictionary/copy_methods.py
@@ -5,8 +5,8 @@ print('d1=',d1)
 
 
 
-# d={'name':'agnaey','age':21,'mark':30}
-# d1=d.copy()
-# d1.pop('name')
-# print('d=',d)
-# print('d1=',d1)
+d={'name':'agnaey','age':21,'mark':30}
+d1=d.copy()
+d1.pop('name')
+print('d=',d)
+print('d1=',d1)

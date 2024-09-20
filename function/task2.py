612212cbab4fa13b867ea3e828005efccbcf5709
agnaey <agnaeyviswanath1234@gmail.com>
2024-09-20 13:35:39 +0530

    task2 done

--- function/task2.py
+++ function/task2.py
@@ -1,5 +1,5 @@
-lib=[]
-book=[]
+lib=[{'id': 101, 'name': 'agnaey', 'email': 'a', 'address': 'aaa', 'mobile': 36345, 'password': '1234','book':[]}]
+book=[{'id': 101, 'name': 'one piece', 'stock': 20, 'price': 2000},{'id': 102, 'name': 'naruto', 'stock': 15, 'price': 1500}]
 
 
 def register():    
@@ -20,7 +20,7 @@ def register():
         address=input('enter your address :')
         mobile=int(input('enter your mobile :'))
         password=input('enter your password :')
-        lib.append({'id':id,'name':name,'email':email,'address':address,'mobile':mobile,'password':password})
+        lib.append({'id':id,'name':name,'email':email,'address':address,'mobile':mobile,'password':password,'book':[]})
         print('------registered------')
 def login():
     username=input('enter username :')
@@ -48,10 +48,10 @@ def add_book():
     print('-------book added-------')
 
 def view_book():
-    print('{:<10}{:<10}{:<10}{:<10}'.format('id','name','stock','price'))
+    print('{:<10}{:<20}{:<10}{:<10}'.format('id','name','stock','price'))
     print('-'*40)
     for i in book:
-        print('{:<10}{:<10}{:<10}{:<10}'.format(i['id'],i['name'],i['stock'],i['price']))
+        print('{:<10}{:<20}{:<10}{:<10}'.format(i['id'],i['name'],i['stock'],i['price']))
 
 def update_book():
     id=input('enter your id: ')
@@ -82,10 +82,58 @@ def delete_book():
             print('-----invalid id------')
 
 def view_users():
+    print('-'*50)
     print('{:<10}{:<10}{:<10}{:<10}'.format('id','name','email','address','mobile'))
-    print('-'*40)
+    print('-'*50)
     for i in lib:
         print('{:<10}{:<10}{:<10}{:<10}'.format(i['id'],i['name'],i['email'],i['address'],i['mobile']))
+
+def view_pro(user):
+    # print(user)
+    print('-'*50)
+    print('{:<10}{:<10}{:<10}{:<10}'.format('id','name','email','address','mobile'))
+    print('-'*50)
+    print('{:<10}{:<10}{:<10}{:<10}'.format(user['id'],user['name'],user['email'],user['address'],user['mobile']))
+
+def view_book():
+    print('-'*50)
+    print('{:<10}{:<20}{:<10}{:<10}'.format('id','name','stock','price'))
+    print('-'*50)
+    for i in book:
+        print('{:<10}{:<20}{:<10}{:<10}'.format(i['id'],i['name'],i['stock'],i['price']))
+
+def book_take(user):
+    id=int(input('enter book id you want to add: '))
+    f=0
+    for i in book:
+        if i['id']==id:
+            f=1
+            if i['stock']>0:
+                user['book'].append(i['id'])
+                i['stock']-=1
+                print('----book added----')
+            else:
+                print('-----out of stock------')
+    if f==0:
+            print('-----id not found--------')
+
+def return_book(user):
+    id=int(input('enter book id you want to return: '))
+    f=0
+    for i in book:
+        if i['id']==id and id in user['book']:
+            f=1
+            i['stock']+=1
+            user['book'].remove(id)
+            print('--------book returned-------')
+        # else:
+        #     print('-----book id invalid-----')
+    if f==0:
+        print('-----book not found------')
+
+def book_in_hand(user):
+    print(user['book'])
+
 
 
 while True:
@@ -128,7 +176,29 @@ while True:
                 else:
                     break
         elif f==2:
-            print('---------user login----------')
+            # print('---------user login----------')
+            while True:
+                print('''
+                      1.view profile
+                      2.view book
+                      3.book take
+                      4.return book
+                      5.book in hand
+                      6.exit
+
+''')
+                sub_ch=int(input('enter your choise:'))
+                if sub_ch==1:
+                    view_pro(user)
+                elif sub_ch==2:
+                    view_book()
+                elif sub_ch==3:
+                    book_take(user)
+                elif sub_ch==4:
+                    return_book(user)
+                elif sub_ch==5:
+                    book_in_hand(user)
+                else:break
 
 
         else:print('------invalid username or password-------')

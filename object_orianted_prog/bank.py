c746316eef50ea50b4a05be0e8ba15e226c57e45
agnaey <agnaeyviswanath1234@gmail.com>
2024-09-27 13:28:38 +0530

    object orianted added

---
+++ object_orianted_prog/bank.py
@@ -0,0 +1,32 @@
+##                    positional argumet
+
+# class bank:
+#     def __init__(self,min):
+#         self.bal=min
+#     def deposit(self,amt):
+#         self.bal+=amt
+#         print(amt,'amount added')
+#     def withdraw(self,amt):
+#         self.bal-=amt
+#         print(amt,'withdraw')
+#     def display(self):
+#         print('display',self.bal)
+
+
+# user1=bank(500)
+# user1.deposit(1000)
+# user1.withdraw(500)
+# user1.display()
+
+# user2=bank(1000)
+# user2.deposit(5000)
+# user2.withdraw(400)
+# user2.display()
+
+# --------------------------------------------
+
+class demo:
+    def __init__(self,a):
+        print(a)
+obj=demo(12)
+        
---
+++ object_orianted_prog/synefo.py
@@ -0,0 +1,9 @@
+class synnefo:
+    def python():
+        print('python in synefo')
+    def php():
+        print('php in synefo')
+std1=synnefo
+std1.python()
+
+        
